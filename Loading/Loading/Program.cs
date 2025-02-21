41bffb8259de0c3fad8dbf3518c390fdf825dd40
limjr1123 <63219254+limjr1123@users.noreply.github.com>
2025-02-21 15:04:53 +0900

    day2_6

---
+++ Loading/Loading/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Loading
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            String square = "□□□□□□□□□□";
+            char replacement = '■';
+            for (int i = 0 ; i < square.Length; i++)
+            {
+                Console.WriteLine(square);
+                Thread.Sleep(500); //1000 1초다 (1000ms)
+                Console.Clear();    //콘솔 화면 지우기
+
+                square = square.Remove(i, 1).Insert(i, replacement.ToString());
+            }
+        }
+    }
+}
