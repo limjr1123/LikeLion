2263c5a462d214e1a4cf58693c7713b5de9859a8
limjr1123 <63219254+limjr1123@users.noreply.github.com>
2025-02-21 13:13:41 +0900

    day2_4

---
+++ studdy4/studdy4/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace studdy4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //// 문자열
+            //String greeting;            // 문자열 변수를 선언
+            //greeting = "Hello, World!"; // 변수에 값을 저장
+
+            //// 변수의 값을 사용
+            //Console.WriteLine(greeting);    // 출력 : Hello, World!
+
+            //// 변수 선언과 초기화를 한번에 수행
+            //int score = 100;            // 정수형 변수 선언과 동시에 100으로 초기화
+            //double temperature = 36.6;  // 실수형 변수 선언과 초기화
+            //string city = "Seoul";      // 문자열 변수 선언과 초기화
+
+            //// 변수 출력
+            //Console.WriteLine(score);       // 출력 : 100
+            //Console.WriteLine(temperature); // 출력 : 36.6
+            //Console.WriteLine(city);        // 출력 : Seoul
+
+            //// 같은 데이터 타입의 변수를 쉼표로 구분해서 선언
+            //int x = 10, y = 20, z = 30; // 정수형 변수 x,y,z를 선언하고 초기화
+            //Console.WriteLine(x);
+            //Console.WriteLine(y);
+            //Console.WriteLine(z);
+
+
+            ////변경 불가능한 변수
+            //const double Pi = 3.14159;  // 상수 pi 선언 및 초기화
+            //const int MaxScore = 100;   // 정수형 상수 선언
+            
+            //Console.WriteLine("Pi : " + Pi);
+            //Console.WriteLine("MaxScore : " + MaxScore);
+
+            //int att = 16755;
+            //int MaxHp = 78103;
+            //int critical = 36;
+            //int specialization = 1017;
+            //int overpower = 41;
+            //int speed = 611;
+            //int Patience = 22;
+            //int skillful = 39;
+
+            //Console.WriteLine("공격력 : " + att);
+            //Console.WriteLine("최대생명력 : " + MaxHp);
+            //Console.WriteLine("치명 : " + critical);
+            //Console.WriteLine("특화 : " + specialization);
+            //Console.WriteLine("제압 : " + overpower);
+            //Console.WriteLine("신속 : " + speed);
+            //Console.WriteLine("인내 : " + Patience);
+            //Console.WriteLine("숙련 : " + skillful);
+
+
+        }
+    }
+}
