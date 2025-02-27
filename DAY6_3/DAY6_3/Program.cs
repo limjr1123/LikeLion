9ebb5656c28cfaaeec0080fb13019713c7d88046
limjr1123 <63219254+limjr1123@users.noreply.github.com>
2025-02-27 11:02:48 +0900

    namespace

---
+++ DAY6_3/DAY6_3/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//네임스페이스
+//클래스, 함수 ,변수 이름이 충돌하는것을 방지하기 위해 사용된다.
+//
+
+namespace Mynamespace
+{
+    class MyClass
+    {
+        public static void SayHello()
+        {
+            Console.WriteLine("안녕하세요! Mynamespace의 MyClass입니다.");
+        }
+    }
+}
+namespace DAY6_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Mynamespace.MyClass.SayHello();
+        }
+    }
+}
