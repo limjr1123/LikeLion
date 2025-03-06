e1fd36bfb8a54eda0042f959db69bea3500a5514
limjr1123 <63219254+limjr1123@users.noreply.github.com>
2025-03-06 15:13:14 +0900

    RPG

---
+++ DAY10_TEXTRPG/DAY10_TEXTRPG/MainGame.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAY10_TEXTRPG
+{
+    class MainGame
+    {
+
+        public Player m_pPlayer = null;
+        //필드 객체
+        public Field m_pField = null;
+
+        //초기화 해주는 함수
+        public void initiallize()
+        {
+            //플레이어 생성 및 직업 선택
+            m_pPlayer = new Player();
+            m_pPlayer.SelectJob();
+        }
+
+        public void Progress()
+        {
+            int iInput = 0;
+
+            while (true)
+            {
+                Console.Clear();
+                m_pPlayer.Render(); //플레이어 출력
+                Console.WriteLine("1.사냥터  2.종료 : ");
+                iInput = int.Parse(Console.ReadLine());
+
+                if (iInput == 2)
+                    break;
+                if(iInput ==1)
+                {
+                    //사냥터 구현
+                    if(m_pField == null)
+                    {
+                        m_pField = new Field();
+                        m_pField.SetPlayer(ref m_pPlayer);
+                    }
+                    m_pField.Progress();
+                }
+            }
+        }
+        public MainGame() { }
+        ~MainGame() { }
+
+    }
+}
---
+++ DAY10_TEXTRPG/DAY10_TEXTRPG/Monster.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAY10_TEXTRPG
+{
+    public class Monster
+    {
+        public INFO m_tMonster; //몬스터 데이터
+
+        public void SetDamage(int iAttack) { m_tMonster.iHp -= iAttack; } //들어오는 인자값으로  hp갑소
+        public void SetMonster(INFO tMonster) { m_tMonster = tMonster; }
+        public INFO GetMonster() { return m_tMonster; }
+
+        public void Render()
+        {
+            Console.WriteLine("+====================");
+            Console.WriteLine("몬스터 이름 : " + m_tMonster.strName);
+            Console.WriteLine("체력 : " + m_tMonster.iHp + "공격력 : " + m_tMonster.iAttack);
+            Console.WriteLine("+====================");
+        }
+
+        public Monster()
+        {
+
+        }
+        ~Monster() { }
+    }
+}
