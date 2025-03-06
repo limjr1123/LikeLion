d11f7f55fa183c22bf159221ba24cd1341696b23
limjr1123 <63219254+limjr1123@users.noreply.github.com>
2025-03-06 16:43:53 +0900

    interface2

---
+++ DAY10_6/DAY10_6/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAY10_6
+{
+    interface IPayment
+    {
+        void ProcessPayment();
+    }
+    //신용카드 결제 클래스
+    class CreditCardPayment : IPayment
+    {
+        public void ProcessPayment()
+        {
+            Console.WriteLine("신용카드 결제 완료!");
+        }
+    }
+
+    class PayPalPayment : IPayment
+    {
+        public void ProcessPayment()
+        {
+            Console.WriteLine("PayPal 결제완료");
+        }
+    }
+
+    class PaymentProcessor
+    {
+        public void Pay(IPayment payment)
+        {
+            payment.ProcessPayment();
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PaymentProcessor processor = new PaymentProcessor();
+
+            IPayment creditCar = new CreditCardPayment();
+            IPayment paypal = new PayPalPayment();
+
+            processor.Pay(paypal);
+        }
+    }
+}
